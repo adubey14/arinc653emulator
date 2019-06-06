b4569c0639e17d73aef4080acd9d52a044d1074a
abhishek dubey <abhishek.dubey@vanderbilt.edu>
2019-06-06 14:36:09 -0500

    removed files that are not required

--- ACM-src/ACE_CONFIG_FILES/ace_config.h
+++
@@ -1,2 +0,0 @@
-#include "config-linux.h"
-#define ACE_HAS_MONITOR_FRAMEWORK 1
--- build.sh
+++ build.sh
@@ -1,37 +1,8 @@
 #!/bin/bash
 source bootstrap.sh
 
-
-
-cd $CIAO_ROOT
-make $@ 
-
-result=$?
-    script="ACE make $@"
-    if (( $result ))
-    then
-        echo "$script failed"
-        exit 1
-    else
-        echo "$script OK"
-    fi
-
-
-
-cd $DDS_ROOT
-make $@
-result=$?
-    script="DDS make $@"
-    if (( $result ))
-    then
-        echo "$script failed"
-        exit 1
-    else
-        echo "$script OK"
-    fi
 cd $ACM_SRC
 make $@
-
 result=$?
     script="ACM make $@"
     if (( $result ))
@@ -42,10 +13,8 @@ result=$?
         echo "$script OK"
     fi
 
-
 var="$@"
 mresult=`expr match "$var"  ".*clean*"`
-
 if [ "$mresult" == "0" ]; then
 make install
 else
